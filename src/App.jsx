923eac32df0fef6c1d7612b7bce0443cf16c8639
TheOceanV <dataguru.2016@gmail.com>
2021-09-30 00:14:06 +0530

    component life cycle methods

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import User from "./components/user";
+
+function App() {
+  return (
+  <div>
+    <h1>Users</h1>
+    <User name="Pavan" description="FSWD" />
+  </div>
+  );
+}
+
+export default App;
+
