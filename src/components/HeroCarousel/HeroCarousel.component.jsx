dcd7d197bd7b9c26be34aefcd827012f66c856d1
TheOceanV <dataguru.2016@gmail.com>
2021-10-01 23:25:30 +0530

    add entertainment carousel

--- src/components/HeroCarousel/HeroCarousel.component.jsx
+++ src/components/HeroCarousel/HeroCarousel.component.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import HeroSlider from "react-slick";
 
-// Import css files
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// Component
+import { NextArrow, PrevArrow } from "./Arrows.component";
 
 const HeroCarousal = () => {
     const settingsLG = {
@@ -13,7 +12,9 @@ const HeroCarousal = () => {
         centerPadding: "300px",
         infinite: true,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
       };
 
       const settings = {
@@ -22,7 +23,9 @@ const HeroCarousal = () => {
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1  
+        slidesToScroll: 1,  
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
       };
 
       const images = [
