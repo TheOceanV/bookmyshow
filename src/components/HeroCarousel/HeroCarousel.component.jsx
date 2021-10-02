4b5db4f7ebf483a3c5cce36b2a4ddea33aab7c1a
TheOceanV <dataguru.2016@gmail.com>
2021-10-02 14:51:56 +0530

    add TMDB api

--- src/components/HeroCarousel/HeroCarousel.component.jsx
+++ src/components/HeroCarousel/HeroCarousel.component.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HeroSlider from "react-slick";
+import axios from "axios";
 
 // Component
 import { NextArrow, PrevArrow } from "./Arrows.component";
 
 const HeroCarousal = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect( () => {
+    const requestNowPlayingMovies = async () => {
+      const getImages = await axios.get("/movie/now_playing");
+      setImages(getImages.data.results);
+    };   
+
+    requestNowPlayingMovies();
+  }, []);
+
     const settingsLG = {
         arrows: true,
         autoplay: true,
@@ -26,14 +38,6 @@ const HeroCarousal = () => {
         nextArrow: <NextArrow />,
         prevArrow: <PrevArrow />,
       };
-
-      const images = [
-          "https://images.unsplash.com/photo-1593642533144-3d62aa4783ec?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-          "https://images.unsplash.com/photo-1632952745637-a104d96d8405?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-          "https://images.unsplash.com/photo-1633085650319-b9efc57dcf07?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-          "https://images.unsplash.com/photo-1633086218123-e368e0e71443?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-          "https://images.unsplash.com/photo-1632952297232-0548107fdab3?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1M3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-         ];
     
     return (
         <>
@@ -41,7 +45,11 @@ const HeroCarousal = () => {
               <HeroSlider {...settings}>
                  {images.map((image) => (
                      <div className="w-full h-56 md:h-80 py-3">
-                        <img src={image} alt="testing" className="w-full h-full" />    
+                        <img 
+                           src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`} 
+                           alt="testing" 
+                           className="w-full h-full" 
+                        />    
                      </div>
                  ))}   
               </HeroSlider>          
@@ -52,7 +60,7 @@ const HeroCarousal = () => {
               {images.map((image) => (
                   <div className="w-full h-96 px-2 py-3">
                     <img
-                      src={image}
+                      src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
                       alt="testing"
                       className="w-full h-full rounded-md"
                       />    
--- src/components/Navbar/MovieNavbar.component.jsx
+++ src/components/Navbar/MovieNavbar.component.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  BiChevronRight,
   BiSearch,
   BiMenu,
   BiChevronDown,
