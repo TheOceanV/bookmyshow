338542ccbf1d4286bfb15fe5135333b7fd1ccf73
TheOceanV <dataguru.2016@gmail.com>
2021-10-02 11:21:10 +0530

    add reusable carousel component

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -2,20 +2,57 @@ import React from "react";
 
 // Components
 import EntertainmentCardSlider from "../components/Entertainment/EntertainmentCard.component";
-import Premier from "../components/Premier/Premier.component";
+import PosterSlider from "../components/PosterSlider/PosterSlider.component";
+
+// config
+import TempPosters from "../config/TempPosters.config";
 
 const HomePage = () => {
     return (
         <>
-         <div className="container mx-auto px-4">
+         <div className="flex flex-col gap-10">
+         <div className="container mx-auto px-4 ">
             <h1 className="text-2xl font-bold text-gray-800 my-3">
               The Best of Entertainment
             </h1>
             <EntertainmentCardSlider />
-            <Premier />
          </div>
-        </>
-    );
+         
+         <div className="bg-bms-800 py-12">
+           <div className="container mx-auto px-4 flex flex-col gap-3">
+             <div className=" hidden md:flex">
+               <img
+                 src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+                 alt="Rupay"
+                 className="w-full h-full"
+               />   
+        </div>
+          <PosterSlider 
+              images={TempPosters} 
+              title="Premieres" 
+              subtitle="Brand New Releases Every Friday"
+              isDark 
+          />
+        </div>
+    </div> 
+ </div>
+
+      <div className=" container mx-auto px-4 my-8">
+         <PosterSlider 
+              images={TempPosters} 
+              title="Online Streaming Events" 
+              isDark={false}
+          />
+      </div>
+      <div className=" container mx-auto px-4 my-8">
+         <PosterSlider 
+              images={TempPosters} 
+              title="Outdoor Events" 
+              isDark={false}
+          />
+      </div>
+   </>
+  );
 };
 
 export default HomePage;
